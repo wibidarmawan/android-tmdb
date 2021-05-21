ad26f8d71abddfd62236dd5eb4eda26c41de8c72
Christianto Wibisono Darmawan <31921484+wibidarmawan@users.noreply.github.com>
2021-05-21 10:13:49 +0700

    Delete loginapi directory

--- loginapi/src/main/java/com/digi/loginapi/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.digi.loginapi;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserModel, Integer>{
-	UserModel getUserModelByEmail(String email);	
-}
